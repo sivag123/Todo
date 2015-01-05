ce1de3ec76c4dabd1b41ddc58f728bc3e0feb606
Siva G <siva.g@freshdesk.com>
2015-01-05 16:57:22 +0530

    created task model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105110953) do
+ActiveRecord::Schema.define(version: 20150105112623) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "user_id",     limit: 4
+    t.string   "description", limit: 255
+    t.string   "status",      limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        limit: 255
